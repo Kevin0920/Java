9c746680d9de93f853ebc44808777d4bee07fb19
Lingsi Ke <lingsike@Lingsis-MacBook-Pro.local>
2017-07-31 13:31:36 -0700

    Java_Fund

---
+++ javaFun/HelloWorld.java
@@ -0,0 +1,7 @@
+// need to make sure the class name needs to match the file name
+
+public class HelloWorld {
+  public static void main(String[] args) {
+      System.out.println("Hello World");
+  }
+}
---
+++ javaPro/FirstPro.java
@@ -0,0 +1,8 @@
+
+public class FirstPro{
+  public static void main(String[] args) {
+    System.out.println("My name is Kevin Ke");
+    System.out.println("I am 18 years old");
+    System.out.println("My hometown is China");
+  }
+}
